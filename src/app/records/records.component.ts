e4d35ec144a9679f56593b673b8f3a6af3f4ec2f
zbuchheit <zbchht@gmail.com>
2022-04-26 20:04:11 -0600

    linting error

--- src/app/records/records.component.ts
+++ src/app/records/records.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
